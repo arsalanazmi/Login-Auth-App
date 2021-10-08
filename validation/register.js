153dd145583b6e02fe232fd42e6f0733fee3aa8a
arsalanazmi <arsalan.azmi92@gmailcom>
2021-10-09 01:35:22 +0500

    backend validation

---
+++ validation/register.js
@@ -0,0 +1,69 @@
+const Validator = require("validator");
+const isEmpty = require("is-empty");
+
+module.exports = function validateRegisterInput(data) {
+  let errors = {};
+
+  // Convert empty fields to an empty string so we can use validator functions
+  data.firstName = !isEmpty(data.firstName) ? data.firstName : "";
+  data.lastName = !isEmpty(data.lastName) ? data.lastName : "";
+  data.gender = !isEmpty(data.gender) ? data.gender : "";
+  data.phone = !isEmpty(data.phone) ? data.phone : "";
+  data.dob = !isEmpty(data.dob) ? data.dob : "";
+  data.email = !isEmpty(data.email) ? data.email : "";
+  data.password = !isEmpty(data.password) ? data.password : "";
+  data.confirmPassword = !isEmpty(data.confirmPassword) ? data.confirmPassword : "";
+
+  // Name checks
+  if (Validator.isEmpty(data.firstName)) {
+    errors.firstName = "First name is required";
+  }
+  if (Validator.isEmpty(data.lastName)) {
+    errors.lastName = "Last name is required";
+  }
+
+  // Gender checks
+  if (Validator.isEmpty(data.gender)) {
+    errors.gender = "Gender field is required";
+  }
+
+  // Phone checks
+  if (Validator.isEmpty(data.phone)) {
+    errors.phone = "Phone field is required";
+  }
+  //   if (validator.isMobilePhone(data.phone.toString(),'en-PK')) {
+  //     errors.phone = "Phone is invalid";
+  //   }
+
+  //Date Of Birth checks
+  if (Validator.isEmpty(data.dob)) {
+    errors.dob = "Date of Birth is required";
+  }
+
+  // Email checks
+  if (Validator.isEmpty(data.email)) {
+    errors.email = "Email field is required";
+  } else if (!Validator.isEmail(data.email)) {
+    errors.email = "Email is invalid";
+  }
+
+  // Password checks
+  if (Validator.isEmpty(data.password)) {
+    errors.password = "Password field is required";
+  }
+  if (Validator.isEmpty(data.confirmPassword)) {
+    errors.confirmPassword = "Confirm password field is required";
+  }
+  if (!Validator.isLength(data.password, { min: 6, max: 20 })) {
+    errors.password =
+      "Password must be at least 6 characters & maximum of 30 characters";
+  }
+  if (!Validator.equals(data.password, data.confirmPassword)) {
+    errors.confirmPassword = "Passwords must match";
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
